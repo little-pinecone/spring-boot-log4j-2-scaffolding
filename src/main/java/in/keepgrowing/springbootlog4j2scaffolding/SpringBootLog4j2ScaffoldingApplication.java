802d67b9f3dea8cdeaaa1664b69cc9ed4744e668
little-pinecone <marta.szymek.mail@gmail.com>
2020-04-09 16:19:46 +0200

    [feature/KG-11] Initial commit

---
+++ src/main/java/in/keepgrowing/springbootlog4j2scaffolding/SpringBootLog4j2ScaffoldingApplication.java
@@ -0,0 +1,13 @@
+package in.keepgrowing.springbootlog4j2scaffolding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLog4j2ScaffoldingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLog4j2ScaffoldingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/keepgrowing/springbootlog4j2scaffolding/SpringBootLog4j2ScaffoldingApplicationTests.java
@@ -0,0 +1,13 @@
+package in.keepgrowing.springbootlog4j2scaffolding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLog4j2ScaffoldingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
